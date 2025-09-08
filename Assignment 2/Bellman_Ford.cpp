955be98bfedb2f1920d88a46c3b0e14198578457
Neevs1 <78549886+Neevs1@users.noreply.github.com>
2025-09-08 13:51:30 +0530

    Add files via upload

--- Assignment 2/Bellman_Ford.cpp
+++ Assignment 2/Bellman_Ford.cpp
@@ -27,6 +27,7 @@ class Bf{
         int src;
         cin>>src;
         vector<int> output(V,INT_MAX);
+        vector<int> parent(V);
         output[src]=0;
         for(int i=1;i<=V-1;i++){
             for(auto edge:edges){
@@ -35,6 +36,7 @@ class Bf{
                 int weight=edge.weight;
                 if(output[u]!=INT_MAX && output[u]+weight<output[v]){
                     output[v]=output[u]+weight;
+                    parent[v]=u;
                 }
             }
         }
@@ -47,13 +49,13 @@ class Bf{
                 return;
             }
         }
-        cout<<"Vertex Distance from Source"<<endl;
+        cout<<"Vertex\tParent\tDistance from Source"<<endl;
         for(int i=0;i<V;i++){
             if(output[i]==INT_MAX){
-                cout<<i<<"\t\t"<<"INF"<<endl;
+                cout<<i<<"\t"<<parent[i]<<"\t"<<"INF"<<endl;
             }
             else{
-                cout<<i<<"\t\t"<<output[i]<<endl;
+                cout<<i<<"\t"<<parent[i]<<"\t"<<output[i]<<endl;
             }
         }
 
@@ -74,27 +76,58 @@ int main(){
 
 /*
  Sample Input and Output:
-    Enter number of vertices
-    5
-    Enter number of edges
-    5
-    Enter edges in the format src dest weight
-    Enter edge 1
-    1 3 2
-    Enter edge 2
-    4 3 -1
-    Enter edge 3
-    2 4 1
-    Enter edge 4
-    1 2 1
-    Enter edge 5
-    0 1 5
-    Enter source vertex
-    0
-    Vertex Distance from Source
-    0               0
-    1               5
-    2               6
-    3               6
-    4               7
+   Enter number of vertices
+5
+Enter number of edges
+10
+Enter edges in the format src dest weight
+Enter edge 1
+0 1 6
+Enter edge 2
+0 3 7
+Enter edge 3
+1 2 5
+Enter edge 4
+1 4 -4
+Enter edge 5
+1 3 8
+Enter edge 6
+2 1 -2
+Enter edge 7
+4 2 7
+Enter edge 8
+4 0 2
+Enter edge 9
+3 2 -3
+Enter edge 10
+3 4 9
+Enter source vertex
+0
+Vertex	Parent	Distance from Source
+0	0	0
+1	2	2
+2	3	4
+3	0	7
+4	1	-2
+
+
+Enter number of vertices
+4
+Enter number of edges
+5
+Enter edges in the format src dest weight
+Enter edge 1
+0 1 4
+Enter edge 2
+0 3 5
+Enter edge 3
+1 3 5
+Enter edge 4
+3 2 3
+Enter edge 5
+2 1 -10
+Enter source vertex
+0
+Graph contains negative weight cycle
+
 */
